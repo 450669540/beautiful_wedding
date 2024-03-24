46f6a48a92137670b83ed1b35d659acd3a994a39
zhuyingjie <zhuyingjie@xueji.com>
2024-03-24 22:13:38 +0800

    feat:修改数据库地址

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:51:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-03-24 22:05:50
+ * @LastEditTime: 2024-03-24 22:13:18
  * @FilePath: /beautiful-wedding/router/user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -117,6 +117,16 @@ router.post('/updateUserInfo', async (req, res) => {
     { open_id: req?.session?.user?.open_id },
     { nick_name, avatar }
   );
+  res.send({
+    msg: 'get请求成功',
+    code: 1,
+    success: true,
+    data,
+  });
+});
+
+router.get('/deleteUserInfo', async (req, res) => {
+  const data = await userOperate.deleteData({ open_id });
   res.send({
     msg: 'get请求成功',
     code: 1,
