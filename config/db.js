cc9b579e1e045353d26a0b05a4cabb5a452a5d4d
zhuyingjie <zhuyingjie@xueji.com>
2024-03-24 21:32:04 +0800

    feat:修改数据库地址

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-18 16:01:32
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-03-24 20:17:26
+ * @LastEditTime: 2024-03-24 21:31:54
  * @FilePath: /beautifu-wedding/config/db.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var mongoose = require('mongoose');
 
-var db = mongoose.createConnection('mongodb://101.37.68.92:27017/admin', {
+var db = mongoose.createConnection('mongodb://127.0.0.1:27017/admin', {
   useNewUrlParser: true,
 
   useUnifiedTopology: true,
