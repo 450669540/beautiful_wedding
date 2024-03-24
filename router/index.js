31d7bee35cb2b5ef7e3b2fc195214e5e7d12edd4
zhuyingjie <zhuyingjie@xueji.com>
2024-03-24 21:43:22 +0800

    feat:修改数据库地址

--- router/index.js
+++ router/index.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-18 14:00:35
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-02-19 11:57:10
+ * @LastEditTime: 2024-03-24 21:43:03
  * @FilePath: /beautifu-wedding/router/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,7 +31,7 @@ router.get('/addAdvertisements', async (req, res) => {
     const data = await advertisementOperate.create({
       _id: '57657', // 手动指定_id值
       code: 'MY_BANNER',
-      url: 'http://127.0.0.1:5010/p2.png',
+      url: 'http://101.37.68.92:5010/p2.png',
       order: 1,
     });
 
--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:51:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-03-13 16:14:24
+ * @LastEditTime: 2024-03-24 21:43:10
  * @FilePath: /beautiful-wedding/router/user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -139,7 +139,8 @@ router.post('/uploadAvatar', async (req, res) => {
       files?.fileData?.[0]?.newFilename
     );
     //这里我传回一个下载此图片的Url
-    var downUrl = 'http://127.0.0.1:5010/' + files?.fileData?.[0]?.newFilename; //这里是想传回图片的链接
+    var downUrl =
+      'http://101.37.68.92:5010/' + files?.fileData?.[0]?.newFilename; //这里是想传回图片的链接
     fs.rename(oldPath, newPath, () => {
       //fs.rename重命名图片名称
       res.send({
