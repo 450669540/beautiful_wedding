f598275e9a847dee18541f553a3af11c6103f561
zhuyingjie <zhuyingjie@xueji.com>
2024-03-24 21:58:45 +0800

    feat:修改数据库地址

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:51:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-03-24 21:43:10
+ * @LastEditTime: 2024-03-24 21:57:15
  * @FilePath: /beautiful-wedding/router/user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -55,6 +55,7 @@ router.post('/login', async (req, res) => {
         data: userinfo?.[0],
       });
     } else {
+      console.log('创建新用户');
       // 创建用户
       let creatUserinfoStatus = await userOperate?.create({
         _id: uuid.v4(),
