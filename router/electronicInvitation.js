ba7c0b3c909c05882e796ab8bb9f060f92601287
zhuyingjie <zhuyingjie@xueji.com>
2024-04-02 15:13:20 +0800

    feat:新增删除电子请柬接口

--- router/electronicInvitation.js
+++ router/electronicInvitation.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-03-25 16:28:01
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-02 15:09:38
+ * @LastEditTime: 2024-04-02 15:13:12
  * @FilePath: /beautiful-wedding/router/electron.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -58,8 +58,8 @@ router.get('/getElectronicInvitationById', async (req, res) => {
 /** 删除指定请柬 */
 router.get('/deleteElectronicInvitationById', async (req, res) => {
   const query = req.query;
-  let data = await electronTemplateOperate.findOne({ _id: query?.id });
-  if (data) {
+  let result = await electronTemplateOperate.findOne({ _id: query?.id });
+  if (result) {
     const data1 = await electronicInvitationComponentOperate.deleteData(
       'deleteOne',
       {
