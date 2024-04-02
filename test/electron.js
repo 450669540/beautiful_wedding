f5ee2c1ebe4711d332783bb13394e7d3b902038f
zhuyingjie <zhuyingjie@xueji.com>
2024-04-02 15:37:51 +0800

    feat:插入电子请柬模版修改

--- test/electron.js
+++ test/electron.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-03-25 16:42:25
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-02 14:19:59
+ * @LastEditTime: 2024-04-02 15:36:29
  * @FilePath: /beautiful-wedding/test/electron.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -65,7 +65,28 @@ const create = async () => {
               },
             ],
           },
+          {
+            type: 'singleImage',
 
+            background: '#f3f4f5',
+            width: '100vw',
+            height: '50vh',
+            images: [
+              {
+                url: 'http://image109.360doc.com/DownloadImg/2018/08/1318/141247485_16_20180813064528834.gif',
+                styles: {
+                  width: '100vw',
+                  height: '100vh',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+              },
+            ],
+            url: '',
+            margin: '24rpx',
+            borderRaduis: '16rpx',
+          },
           {
             type: 'multi',
             value: '',
@@ -335,7 +356,7 @@ const create = async () => {
 
     name: '喜嫁风', //模版名称
 
-    cover: 'https://www.beautifulwedding.cn/example3_3.jpg', //封面
+    cover: 'https://www.beautifulwedding.cn/example1_1.jpg', //封面
 
     type: 'flip', //模版类型
 
@@ -357,4 +378,408 @@ const create = async () => {
   });
 };
 
+const create2 = async () => {
+  const res1 = await electronicInvitationComponentOperate.create({
+    _id: uuid.v4(),
+
+    content: JSON.stringify([
+      {
+        page_id: '3',
+        list: [
+          {
+            type: 'calendar',
+            url: `https://www.beautifulwedding.cn/example1_1.jpg`,
+            styles: {
+              position: 'absolute',
+              top: 0,
+              left: 0,
+            },
+          },
+          {
+            type: 'countDownComponent',
+            value: '2024-04-01',
+            styles: {
+              position: 'absolute',
+              bottom: 30,
+              left: 0,
+            },
+          },
+        ],
+      },
+    ]),
+  });
+
+  const res2 = await electronicInvitationPageOperate.create({
+    _id: uuid.v4(),
+
+    content: JSON.stringify([
+      {
+        id: '1',
+        styles: {
+          background: '#fff',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        list: [
+          {
+            type: 'singleImage',
+            width: '100vw',
+            height: '100vh',
+
+            images: [
+              {
+                animationClass: 'element3',
+                mode: 'widthFix',
+                url: 'https://www.beautifulwedding.cn/example7_3.jpg',
+                styles: {
+                  width: '100vw',
+                  height: '100vh',
+                },
+              },
+            ],
+          },
+
+          {
+            type: 'multi',
+            value: '',
+            styles: {
+              position: 'absolute',
+              top: '30rpx',
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+              color: '#6A4C4C',
+              width: '100%',
+            },
+            children: [
+              {
+                type: 'text',
+                value: 'WEDDING LOVE ',
+                animationClass: 'element',
+                styles: {
+                  fontSize: '100rpx',
+                  textAlign: 'center',
+                },
+              },
+              {
+                type: 'text',
+                value: `我        的        眼        里`,
+                styles: {
+                  fontSize: '32rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `只        有        你`,
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '60rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `张志明    &     余春娇`,
+                animationClass: 'element8',
+                styles: {
+                  fontSize: '48rpx',
+                  fontStyle: 'italic',
+                  marginTop: '60rpx',
+                },
+              },
+            ],
+          },
+          {
+            type: 'text',
+            value: `见    证    相    爱    的    年    年    岁    岁`,
+            animationClass: 'element9',
+            styles: {
+              fontSize: '32rpx',
+              fontStyle: 'italic',
+              position: 'absolute',
+              bottom: '60rpx',
+              letterSpacing: '16rpx',
+              fontWeight: 200,
+            },
+          },
+        ],
+      },
+      {
+        id: '2',
+        background: '#F5F3F2',
+        list: [
+          {
+            type: 'multi',
+            value: '',
+            styles: {
+              position: 'absolute',
+              top: 0,
+              backgroundColor: '#FDFDFD',
+              padding: '40rpx 40rpx 100rpx 40rpx',
+            },
+            children: [
+              {
+                type: 'singleImage',
+
+                background: '#f3f4f5',
+                width: '100vw',
+                height: '50vh',
+                images: [
+                  {
+                    animationClass: 'element2',
+                    url: 'https://www.beautifulwedding.cn/example7_2.jpg',
+                    mode: 'widthFix',
+                    styles: {
+                      width: '540rpx',
+                    },
+                  },
+                ],
+              },
+              {
+                type: 'text',
+                value: `出现在我的生命里`,
+                animationClass: 'element2',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '40rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `很幸运`,
+                animationClass: 'element2',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '12rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `能在有生之年遇见一个`,
+                animationClass: 'element2',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '12rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `如此爱我的你。`,
+                animationClass: 'element2',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '12rpx',
+                },
+              },
+
+              {
+                type: 'text',
+                value: '',
+                styles: {
+                  width: '490rpx',
+                  height: '4rpx',
+                  backgroundColor: '#8D7470',
+                  marginTop: '100rpx',
+                },
+              },
+            ],
+          },
+          {
+            type: 'multi',
+            value: '',
+            styles: {
+              position: 'absolute',
+              top: 270,
+              right: 30,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            },
+            children: [
+              {
+                type: 'text',
+                value: ``,
+                animationClass: 'element10',
+
+                styles: {
+                  width: '30rpx',
+                  height: '30rpx',
+                  background: '#6A4C4C',
+                },
+              },
+
+              {
+                type: 'text',
+                value: `见证相爱的年年岁岁`,
+                class: 'verticalFont',
+                animationClass: 'element10',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '20rpx',
+                  letterSpacing: '20rpx',
+                },
+              },
+            ],
+          },
+          {
+            type: 'multi',
+            value: '',
+            styles: {
+              position: 'absolute',
+              top: 0,
+              right: 10,
+            },
+            children: [
+              {
+                type: 'text',
+                value: `13`,
+                animationClass: 'element8',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '40rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `/`,
+                animationClass: 'element8',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '12rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `14`,
+                animationClass: 'element8',
+                styles: {
+                  fontSize: '32rpx',
+                  marginTop: '12rpx',
+                },
+              },
+              {
+                type: 'text',
+                value: `YOU ARE APPLE OF MY EYES`,
+                class: 'verticalFont',
+                animationClass: 'element8',
+                styles: {
+                  fontSize: '20rpx',
+                  marginTop: '500rpx',
+                  wordSpacing: '60rpx',
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: '3',
+        background: '#F5F3F2',
+        list: [
+          {
+            type: 'singleImage',
+
+            background: '#f3f4f5',
+            width: '100vw',
+            height: '50vh',
+            images: [
+              {
+                animationClass: 'element2',
+                url: 'https://www.beautifulwedding.cn/example7_5.jpg',
+                mode: 'widthFix',
+                styles: {
+                  width: '100vw',
+                },
+              },
+            ],
+          },
+          {
+            type: 'multi',
+            value: '',
+            styles: {
+              width: '100%',
+              height: '200rpx',
+              display: 'flex',
+              flexDirection: 'column',
+              backgroundColor: '#F6F1F0',
+              color: '#6A4C4C',
+              marginLeft: '40rpx',
+            },
+            children: [
+              {
+                type: 'text',
+                value: `我们的故事藏在画里面`,
+
+                animationClass: 'element10',
+                styles: {
+                  fontSize: '40rpx',
+                  marginTop: '20rpx',
+                  letterSpacing: '20rpx',
+                  fontStyle: 'italic',
+                },
+              },
+              {
+                type: 'text',
+                value: `定格了永远`,
+
+                animationClass: 'element4',
+                styles: {
+                  fontSize: '40rpx',
+                  marginTop: '20rpx',
+                  letterSpacing: '20rpx',
+                  fontStyle: 'italic',
+                  marginLeft: '80rpx',
+                },
+              },
+            ],
+          },
+          {
+            type: 'singleImage',
+
+            background: '#f3f4f5',
+            width: '100vw',
+            height: '50vh',
+            images: [
+              {
+                animationClass: 'element2',
+                url: 'https://www.beautifulwedding.cn/example7_6.jpg',
+                mode: 'widthFix',
+                styles: {
+                  width: '100vw',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ]),
+  });
+
+  electronTemplateOperate.create({
+    _id: uuid.v4(),
+
+    name: '电影唯美风', //模版名称
+
+    cover: 'https://www.beautifulwedding.cn/example7_4.jpg', //封面
+
+    type: 'longImage', //模版类型
+
+    component_id: res1?._id, //组件JSON_ID
+
+    page_id: res2?._id, // 模版页面JSON_ID
+
+    create_on: new Date(), //创建时间
+
+    update_on: new Date(), //修改时间
+
+    create_name: '朱莹洁', //创建者
+
+    start_on: '2024-08-12 00:00:00', // 请柬时间
+
+    address: '安徽省蚌埠市龙子湖区慧', //请柬地址
+
+    title: '王五&李四', //请柬标题
+  });
+};
 create();
+create2();
