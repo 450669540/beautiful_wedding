c596b8404cf7a4ce1d592ba0c1df241328878e43
zhuyingjie <zhuyingjie@xueji.com>
2024-04-02 14:54:45 +0800

    feat:图片的请求路径改为正式的

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:51:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-02 14:17:09
+ * @LastEditTime: 2024-04-02 14:54:31
  * @FilePath: /beautiful-wedding/router/user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -148,7 +148,7 @@ router.post('/uploadAvatar', async (req, res) => {
       files?.fileData?.[0]?.newFilename
     );
     //这里我传回一个下载此图片的Url
-    var downUrl = info.baseUrl + files?.fileData?.[0]?.newFilename; //这里是想传回图片的链接
+    var downUrl = info.baseUrl + '/' + files?.fileData?.[0]?.newFilename; //这里是想传回图片的链接
     fs.rename(oldPath, newPath, () => {
       //fs.rename重命名图片名称
       res.send({
