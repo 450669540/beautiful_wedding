a90037b17c589b61c1ded60f8285db43f34d0564
zhuyingjie <zhuyingjie@xueji.com>
2024-04-03 17:51:40 +0800

    feat:新增系统参数

--- dbmodel/system/SystemSchema.js
+++ dbmodel/system/SystemSchema.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 11:06:45
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-03 17:20:57
+ * @LastEditTime: 2024-04-03 17:51:33
  * @FilePath: /beautiful-wedding/dbmodel/advertisement/StudentSchema.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var systemSchema = new Schema({
+  _id: {
+    type: String,
+  },
   comment_flag: { type: Boolean },
 });
 
