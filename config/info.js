f4521b29b97375810d3ab720c33f1a4499c15e43
zhuyingjie <zhuyingjie@xueji.com>
2024-04-11 10:47:27 +0800

    feat:上传图片使用oss

--- config/info.js
+++ config/info.js
@@ -1,5 +1,17 @@
+/*
+ * @Author: zhuyingjie zhuyingjie@xueji.com
+ * @Date: 2024-02-19 13:52:18
+ * @LastEditors: zhuyingjie zhuyingjie@xueji.com
+ * @LastEditTime: 2024-04-11 10:29:58
+ * @FilePath: /beautiful-wedding/config/info.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports = {
   appId: 'wx43f3ae8aca9565e6',
   secret: '34660563b65a2d10fe9745d552222182',
   baseUrl: 'https://www.beautifulwedding.cn',
+  ossRegion: 'oss-cn-hangzhou',
+  ossAccessKeyId: 'LTAISN4lOl02yixZ',
+  ossAccessKeySecret: 'mKFvXPK7mOy66ToQeDTwkNBXbs5xPJ',
+  ossBucket: 'xiaomianma',
 };
