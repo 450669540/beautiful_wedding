fa54aa100af5bba01192ab8ee5ab29fa0f515831
zhuyingjie <zhuyingjie@xueji.com>
2024-04-11 11:44:01 +0800

    feat:上传图片使用oss

--- config/info.js
+++ config/info.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:52:18
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-11 10:29:58
+ * @LastEditTime: 2024-04-11 11:43:38
  * @FilePath: /beautiful-wedding/config/info.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,4 +14,5 @@ module.exports = {
   ossAccessKeyId: 'LTAISN4lOl02yixZ',
   ossAccessKeySecret: 'mKFvXPK7mOy66ToQeDTwkNBXbs5xPJ',
   ossBucket: 'xiaomianma',
+  ossEndpoint: 'https://xiaomianma.oss-cn-hangzhou.aliyuncs.com',
 };
--- utils/uploadOssUtil.js
+++ utils/uploadOssUtil.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-04-11 10:00:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-11 11:23:25
+ * @LastEditTime: 2024-04-11 11:43:51
  * @FilePath: /beautiful-wedding/utils/uploadOssUtil.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,6 +20,7 @@ async function uploadFile(file) {
     accessKeyId: globalInfo?.ossAccessKeyId,
     accessKeySecret: globalInfo?.ossAccessKeySecret,
     bucket: globalInfo?.ossBucket,
+    endpoint: globalInfo?.ossEndpoint,
   });
 
   try {
