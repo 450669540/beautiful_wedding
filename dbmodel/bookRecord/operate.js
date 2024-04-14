cb85cc5a6735f1ff394ff0deaef9f39513d768fb
zhuyingjie <zhuyingjie@xueji.com>
2024-04-14 14:14:51 +0800

    feat:新增礼金簿礼金记录删除接口,修改总记录,权限等接口

--- dbmodel/bookRecord/operate.js
+++ dbmodel/bookRecord/operate.js
@@ -2,17 +2,17 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 11:09:59
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-14 13:33:34
+ * @LastEditTime: 2024-04-14 14:14:05
  * @FilePath: /beautiful-wedding/dbmodel/advertisement/operate.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const giftBookModel = require('./GiftBookModel');
+const bookRecordModel = require('./BookRecordModel');
 
 //查询全部
 const find = (query, start, pageSize) => {
   return new Promise((resolve, reject) => {
     try {
-      giftBookModel
+      bookRecordModel
         .find(query)
         .skip(start)
         .limit(pageSize)
@@ -30,7 +30,7 @@ const find = (query, start, pageSize) => {
 // 富查询条件，对象格式，键值对
 const findOne = (query) => {
   return new Promise((resolve, reject) => {
-    giftBookModel.findOne(query).then((doc) => {
+    bookRecordModel.findOne(query).then((doc) => {
       console.log(doc);
       resolve(doc);
     });
@@ -40,7 +40,7 @@ const findOne = (query) => {
 // 新增操作(save | create 方法)
 const create = (value) => {
   return new Promise((resolve, reject) => {
-    giftBookModel.create(value).then((doc) => {
+    bookRecordModel.create(value).then((doc) => {
       console.log(doc);
       resolve(doc);
     });
@@ -50,7 +50,7 @@ const create = (value) => {
 // 更新操作(update | updateOne | updateMany 方法)
 const update = (query, value) => {
   return new Promise((resolve, reject) => {
-    giftBookModel.updateOne(query, value).then((doc) => {
+    bookRecordModel.updateOne(query, value).then((doc) => {
       console.log(doc);
       resolve(doc);
     });
@@ -61,7 +61,7 @@ const update = (query, value) => {
 const deleteData = (deleteType, query) => {
   return new Promise((resolve, reject) => {
     deleteType = deleteType || 'deleteOne';
-    giftBookModel[deleteType](query).then((doc) => {
+    bookRecordModel[deleteType](query).then((doc) => {
       console.log(doc);
       resolve(doc);
     });
