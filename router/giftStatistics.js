634dcbe0e13bbf4feac12dd06e38266b0203c8da
zhuyingjie <zhuyingjie@xueji.com>
2024-04-14 18:05:59 +0800

    feat:新增礼金簿礼金记录删除接口,修改总记录,权限等接口

--- router/giftStatistics.js
+++ router/giftStatistics.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-03-26 15:48:42
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-14 17:38:16
+ * @LastEditTime: 2024-04-14 18:05:39
  * @FilePath: /beautiful-wedding/router/comment.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -126,10 +126,14 @@ router.post('/updateGiftBookAuthorized', async (req, res) => {
 router.get('/bookRecordList', async (req, res) => {
   const query = req.query;
   const data = await bookRecordOperate.find(
-    {
-      book_id: query?.id,
-      keyword: { $regex: `/${query?.keyword}/i` }, //模糊查询
-    },
+    query?.keyword
+      ? {
+          book_id: query?.id,
+          keyword: { $regex: `/${query?.keyword}/i` }, //模糊查询
+        }
+      : {
+          book_id: query?.id,
+        },
     query?.start,
     query?.pageSize
   );
