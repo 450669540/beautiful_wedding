0070b092b636e9bea106544953301ad587ba4221
zhuyingjie <zhuyingjie@xueji.com>
2024-04-14 13:22:18 +0800

    feat:礼金簿列表查询按时间倒序

--- dbmodel/giftBook/operate.js
+++ dbmodel/giftBook/operate.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 11:09:59
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-14 13:00:12
+ * @LastEditTime: 2024-04-14 13:21:55
  * @FilePath: /beautiful-wedding/dbmodel/advertisement/operate.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,6 +16,7 @@ const find = (query, start, pageSize) => {
         .find(query)
         .skip(start)
         .limit(pageSize)
+        .sort({ timestamp: -1 })
         .then((doc) => {
           console.log(doc);
           resolve(doc);
