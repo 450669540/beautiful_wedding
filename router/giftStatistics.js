fc7ab7710581c871e777fa4ba116c54b668ada80
zhuyingjie <zhuyingjie@xueji.com>
2024-04-15 17:29:07 +0800

    feat:查看到所有拥有礼金簿权限的数据

--- router/giftStatistics.js
+++ router/giftStatistics.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-03-26 15:48:42
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-15 17:23:57
+ * @LastEditTime: 2024-04-15 17:29:02
  * @FilePath: /beautiful-wedding/router/comment.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,6 +18,7 @@ const router = express.Router();
 
 router.get('/giftBookList', async (req, res) => {
   const query = req.query;
+  console.log('当前id', req?.session?.user?._id);
   const data = await giftBookOperate.find(
     { authorized_ids: { $regex: req?.session?.user?._id } },
     query?.start,
