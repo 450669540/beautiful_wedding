96973478f510ca6cb22535e2ca0b96f924bf7bc5
zhuyingjie <zhuyingjie@xueji.com>
2024-04-15 16:12:15 +0800

    feat:礼金簿详情加入权限人员数据,根据userno查询用户

--- dbmodel/giftBook/operate.js
+++ dbmodel/giftBook/operate.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 11:09:59
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-15 15:14:17
+ * @LastEditTime: 2024-04-15 16:12:07
  * @FilePath: /beautiful-wedding/dbmodel/advertisement/operate.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -110,5 +110,4 @@ module.exports = {
   find,
   findOne,
   deleteData,
-  aggregate,
 };
