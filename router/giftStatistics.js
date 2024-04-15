7d7bbd037ab79744ae7f671274cdd04f62cdc80b
zhuyingjie <zhuyingjie@xueji.com>
2024-04-15 17:24:50 +0800

    feat:查看到所有拥有礼金簿权限的数据

--- router/giftStatistics.js
+++ router/giftStatistics.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-03-26 15:48:42
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-15 16:25:09
+ * @LastEditTime: 2024-04-15 17:23:57
  * @FilePath: /beautiful-wedding/router/comment.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,7 +19,7 @@ const router = express.Router();
 router.get('/giftBookList', async (req, res) => {
   const query = req.query;
   const data = await giftBookOperate.find(
-    { user_id: req?.session?.user?._id },
+    { authorized_ids: { $regex: req?.session?.user?._id } },
     query?.start,
     query?.pageSize
   );
--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:51:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-15 16:27:16
+ * @LastEditTime: 2024-04-15 17:21:32
  * @FilePath: /beautiful-wedding/router/user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -124,7 +124,7 @@ router.get('/getUserInfo', async (req, res) => {
 router.get('/getUserInfoByIds', async (req, res) => {
   let { ids } = req.query;
 
-  const data = await userOperate.findOne({
+  const data = await userOperate.find({
     _id: { $in: ids?.split(',') },
   });
 
