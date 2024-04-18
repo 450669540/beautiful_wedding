037a4436d6871ef0eba0958af50c57b3c6bf4694
zhuyingjie <zhuyingjie@xueji.com>
2024-04-18 11:02:30 +0800

    feat:新增接亲游戏接口

--- dbmodel/weddingGame/WeddingGameModel.js
+++ dbmodel/weddingGame/WeddingGameModel.js
@@ -2,13 +2,13 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 11:05:42
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-14 14:13:42
+ * @LastEditTime: 2024-04-18 11:02:18
  * @FilePath: /beautiful-wedding/dbmodel/advertisement/AdvertisementModel.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var db = require('../../config/db.js');
-const bookRecordSchema = require('./BookRecordSchema.js');
+const weddingGameSchema = require('./WeddingGameSchema.js');
 
-let bookRecordModel = db.model('BookRecord', bookRecordSchema);
+let weddingGameModel = db.model('WeddingGame', weddingGameSchema);
 
-module.exports = bookRecordModel;
+module.exports = weddingGameModel;
