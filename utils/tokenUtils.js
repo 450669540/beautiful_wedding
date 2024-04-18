38ac8e68ef4be660af436e976d6dae9b75822368
zhuyingjie <zhuyingjie@xueji.com>
2024-04-18 10:31:53 +0800

    feat:根据token获取用户信息

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-04-18 10:16:36
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-18 10:29:50
+ * @LastEditTime: 2024-04-18 10:31:47
  * @FilePath: /beautiful-wedding/utils/tokenUtils.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,7 @@ const info = require('./../config/info');
 //验证Token
 function verifyToken(authorization) {
   return new Promise((resolve, reject) => {
-    console.log('token', token);
+    console.log('token', authorization);
     const token = authorization.replace('Bearer ', ' ');
 
     try {
