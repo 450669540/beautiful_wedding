8497ef36954780aef6e5c9f9e570d80c4a910850
zhuyingjie <zhuyingjie@xueji.com>
2024-04-19 15:16:27 +0800

    feat:桌子按桌号大小排列

--- router/seat.js
+++ router/seat.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-04-19 10:48:58
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-19 15:14:09
+ * @LastEditTime: 2024-04-19 15:16:21
  * @FilePath: /beautiful-wedding/router/seat.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -132,6 +132,7 @@ router.post('/saveTable', async (req, res) => {
     table_no,
     user_id: tokenRes?.user?._id,
   });
+  console.log('桌数据', result);
   if (result.length > 0) {
     if (!(id && result?.length === 1 && result[0].table_no === table_no)) {
       res.send({
