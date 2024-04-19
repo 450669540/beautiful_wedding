8ded67d3683c55d50aa99f178283f482301499c1
zhuyingjie <zhuyingjie@xueji.com>
2024-04-19 11:22:50 +0800

    feat:新增座位接口

--- dbmodel/table/TableModel.js
+++ dbmodel/table/TableModel.js
@@ -2,13 +2,13 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 11:05:42
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-19 10:46:26
+ * @LastEditTime: 2024-04-19 11:22:42
  * @FilePath: /beautiful-wedding/dbmodel/advertisement/AdvertisementModel.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var db = require('../../config/db.js');
 const tableSchema = require('./TableSchema.js');
 
-let tableModel = db.model('Table', seatArrangementSchema);
+let tableModel = db.model('Table', tableSchema);
 
 module.exports = tableModel;
