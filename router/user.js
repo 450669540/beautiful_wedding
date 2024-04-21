3a263e0df79e3a9d075374dbc2237ffcf4e82497
zhuyingjie <zhuyingjie@xueji.com>
2024-04-21 11:31:48 +0800

    feat:获取婚礼完成进度

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:51:24
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-21 11:28:34
+ * @LastEditTime: 2024-04-21 11:31:42
  * @FilePath: /beautiful-wedding/router/user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -221,7 +221,7 @@ router.post('/updateWeddingDate', async (req, res) => {
 });
 
 /** 获取婚礼进度 */
-router.post('/getWeddingProcess', async (req, res) => {
+router.get('/getWeddingProcess', async (req, res) => {
   let count = 0;
   const gameData = await weddingGameOperate.findOne({
     create_id: req?.session?.user?.open_id,
