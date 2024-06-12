41a93ba571dc506fafdf8424933068ed04034b7f
zhuyingjie <zhuyingjie@xueji.com>
2024-06-12 10:58:24 +0800

    feat:修改oss配置信息

--- config/info.js
+++ config/info.js
@@ -2,7 +2,7 @@
  * @Author: zhuyingjie zhuyingjie@xueji.com
  * @Date: 2024-02-19 13:52:18
  * @LastEditors: zhuyingjie zhuyingjie@xueji.com
- * @LastEditTime: 2024-04-18 09:29:48
+ * @LastEditTime: 2024-06-12 09:54:07
  * @FilePath: /beautiful-wedding/config/info.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,6 +14,6 @@ module.exports = {
   ossRegion: 'oss-cn-hangzhou',
   ossAccessKeyId: 'LTAISN4lOl02yixZ',
   ossAccessKeySecret: 'mKFvXPK7mOy66ToQeDTwkNBXbs5xPJ',
-  ossBucket: 'xiaomianma',
-  ossEndpoint: 'https://xiaomianma.oss-cn-hangzhou.aliyuncs.com',
+  ossBucket: 'beautifulwedding',
+  ossEndpoint: 'https://beautifulwedding.oss-cn-hangzhou.aliyuncs.com',
 };
